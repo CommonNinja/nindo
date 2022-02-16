bed065fdc35d8df8ed0bb0ed2c692d96db85efd4
Daniel Sternlicht <gdblogst@gmail.com>
2022-02-16 16:15:43 +0200

    Using quill from react-quill

--- packages/nindo/src/external/components/richEditor/richEditor.comp.tsx
+++ packages/nindo/src/external/components/richEditor/richEditor.comp.tsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImages } from '@fortawesome/free-solid-svg-icons';
 // @ts-ignore
 import ImageResize from 'quill-image-resize-module-react';
-import * as QuillNamespace from 'quill';
 
 import { AssetsGalleryOpener } from '../assetsGalleryOpener/assetsGalleryOpener.comp';
 
@@ -12,23 +11,21 @@ import 'react-quill/dist/quill.snow.css';
 import './richEditor.scss';
 
 let Icons, TextAlignStyle, Size, Link;
-let Quill: any = QuillNamespace;
-
-Quill.register('modules/imageResize', ImageResize);
+ReactQuill.Quill.register('modules/imageResize', ImageResize);
 
 const iconSvg =
 	'PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IiB2aWV3Qm94PSIwIDAgMTIxLjg2IDEyMi44OCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTIxLjg2IDEyMi44OCIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlIHR5cGU9InRleHQvY3NzIj4uc3Qwe2ZpbGwtcnVsZTpldmVub2RkO2NsaXAtcnVsZTpldmVub2RkO308L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik03Mi4wOSwxOC43Mmg0Mi4zN2MyLjA1LDAsMy44OSwwLjg0LDUuMjIsMi4xOGMxLjM0LDEuMzQsMi4xOCwzLjIsMi4xOCw1LjIydjg5LjM2IGMwLDIuMDUtMC44NCwzLjg5LTIuMTgsNS4yMmMtMS4zNCwxLjM0LTMuMiwyLjE4LTUuMjIsMi4xOEgyNC40OGMtMi4wNSwwLTMuODktMC44NC01LjIyLTIuMThjLTEuMzQtMS4zNC0yLjE4LTMuMi0yLjE4LTUuMjIgVjcxLjQ2YzIuNDcsMSw1LjA1LDEuNzgsNy43MiwyLjI5djIwLjI4aDAuMDNsMCwwQzM3LjcyLDgxLjcsNDYuMjYsNzUuNjEsNTkuMDgsNjUuMmMwLjA1LDAuMDUsMC4xLDAuMSwwLjE1LDAuMTUgYzAuMDMsMC4wMywwLjAzLDAuMDYsMC4wNiwwLjA2bDI2LjgyLDMxLjczbDQuMS0yNS4yNGMwLjI4LTEuNjIsMS44LTIuNzMsMy40Mi0yLjQ1YzAuNjIsMC4wOSwxLjE4LDAuNCwxLjYyLDAuODFsMTguODIsMTkuNzcgVjI3LjkxYzAtMC40LTAuMTYtMC43NS0wLjQ0LTAuOTljLTAuMjUtMC4yNS0wLjYyLTAuNDQtMC45OS0wLjQ0SDc0LjA1QzczLjY0LDIzLjgsNzIuOTgsMjEuMjEsNzIuMDksMTguNzJMNzIuMDksMTguNzJ6IE0zMi43OSwwIEM1MC45LDAsNjUuNTgsMTQuNjgsNjUuNTgsMzIuNzljMCwxOC4xMS0xNC42OCwzMi43OS0zMi43OSwzMi43OUMxNC42OCw2NS41OCwwLDUwLjksMCwzMi43OUMwLDE0LjY4LDE0LjY4LDAsMzIuNzksMEwzMi43OSwweiBNMTUuMzcsMzMuMzdoMTEuMDR2MTUuNzZoMTIuNDVWMzMuMzdoMTEuMzZMMzIuOCwxNi40NEwxNS4zNywzMy4zN0wxNS4zNywzMy4zN0wxNS4zNywzMy4zN3ogTTk0LjI3LDM1LjY2IGMyLjk1LDAsNS42NiwxLjIxLDcuNTgsMy4xNGMxLjk2LDEuOTYsMy4xNCw0LjYzLDMuMTQsNy41OWMwLDIuOTUtMS4yMSw1LjY2LTMuMTQsNy41OGMtMS45NiwxLjk2LTQuNjMsMy4xNC03LjU4LDMuMTQgYy0yLjk1LDAtNS42Ni0xLjIxLTcuNTktMy4xNGMtMS45Ni0xLjk2LTMuMTQtNC42My0zLjE0LTcuNThjMC0yLjk1LDEuMjEtNS42NSwzLjE0LTcuNTlDODguNjUsMzYuODQsOTEuMzIsMzUuNjYsOTQuMjcsMzUuNjYgTDk0LjI3LDM1LjY2TDk0LjI3LDM1LjY2eiIvPjwvZz48L3N2Zz4=';
-Icons = Quill.import('ui/icons');
+Icons = ReactQuill.Quill.import('ui/icons');
 Icons[
 	'uimage'
 ] = `<i class="upload-image-icon" title="Upload Image" aria-hidden="true"><img src="data:image/svg+xml;base64,${iconSvg}" /></i>`;
 Icons.align['left'] = Icons.align[''];
 
-TextAlignStyle = Quill.import('attributors/style/align');
+TextAlignStyle = ReactQuill.Quill.import('attributors/style/align');
 TextAlignStyle.whitelist = ['left', 'center', 'right'];
-Quill.register(TextAlignStyle, true);
+ReactQuill.Quill.register(TextAlignStyle, true);
 
-Size = Quill.import('attributors/style/size');
+Size = ReactQuill.Quill.import('attributors/style/size');
 Size.whitelist = [
 	'8px',
 	'10px',
@@ -39,9 +36,9 @@ Size.whitelist = [
 	'24px',
 	'36px',
 ];
-Quill.register(Size, true);
+ReactQuill.Quill.register(Size, true);
 
-Link = Quill.import('formats/link');
+Link = ReactQuill.Quill.import('formats/link');
 Link.sanitize = function (url: string) {
 	// prefix default protocol.
 	let protocol = url.slice(0, url.indexOf(':'));
@@ -57,7 +54,7 @@ Link.sanitize = function (url: string) {
 		? url
 		: this.SANITIZED_URL;
 };
-Quill.register(Link, true);
+ReactQuill.Quill.register(Link, true);
 
 interface IRichEditorProps {
 	html: string;
