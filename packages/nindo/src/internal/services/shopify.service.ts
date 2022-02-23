66f71749abf60343cb1223a2ae297058e2011dfb
Daniel Sternlicht <gdblogst@gmail.com>
2022-02-23 20:01:12 +0200

    Removing double plugin type from query

--- packages/nindo/src/internal/services/shopify.service.ts
+++ packages/nindo/src/internal/services/shopify.service.ts
@@ -4,14 +4,14 @@ const apiBaseUrl: string = process.env.REACT_APP_PLUGIN_API_URL || '';
 
 class ShopifyService extends HttpService {
 	public async getUserSubscription() {
-		const url = `${apiBaseUrl}/api/v1/shopify/userSubscriptions?pluginType=${this.pluginType}&serviceName=${this.serviceName}&${this.queryParams}`;
+		const url = `${apiBaseUrl}/api/v1/shopify/userSubscriptions?serviceName=${this.serviceName}&${this.queryParams}`;
 		return await this.makeRequest(url);
 	}
 
 	public async createUserSubscription(subscriptionTypeId: string) {
 		const url = `${apiBaseUrl}/shopify/api/${
 			subscriptionTypeId ? 'createSubscription' : 'cancelSubscription'
-		}/${this.pluginType}?pluginType=${this.pluginType}&${this.queryParams}`;
+		}/${this.pluginType}?${this.queryParams}`;
 		return await this.makeRequest(url, {
 			method: 'post',
 			headers: {
