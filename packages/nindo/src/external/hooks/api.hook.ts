7280a5d2769c31098eef91fe33ebd21da31fc869
Daniel Sternlicht <gdblogst@gmail.com>
2022-02-27 16:33:34 +0200

    Import fix

--- packages/nindo/src/external/hooks/api.hook.ts
+++ packages/nindo/src/external/hooks/api.hook.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { HttpService, IHttpResult } from '..';
+import { HttpService, IHttpResult } from '../';
 
 interface IApiResourceState {
   loading: boolean;
