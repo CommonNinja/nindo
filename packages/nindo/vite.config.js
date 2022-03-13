7c642cf154a55fd7c77af82d23f2a730a7f5bbc5
Daniel Sternlicht <gdblogst@gmail.com>
2022-03-13 08:45:32 +0200

    Vite test

--- packages/nindo/vite.config.js
+++ packages/nindo/vite.config.js
@@ -2,26 +2,47 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-dts';
 import typescript from '@rollup/plugin-typescript'
+import react from '@vitejs/plugin-react'
 
 const isExternal = (id) => !id.startsWith('.') && !path.isAbsolute(id);
 const resolvePath = (str) => path.resolve(__dirname, str);
 
 export default defineConfig(() => ({
+	build: {
+		cssCodeSplit: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Nindo',
+			formats: ['es'],
+      fileName: (format) => `index.js`
+    },
+    rollupOptions: {
+      external: isExternal,
+      // output: {
+      //   globals: {
+      //     react: 'React'
+      //   }
+      // }
+    }
+  },
 	// esbuild: {
 	// 	jsxInject: "import React from 'react'",
 	// },
-	build: {
-		lib: {
-			entry: path.resolve(__dirname, 'src/index.ts'),
-			formats: ['es'],
-			fileName: 'index',
-			name: 'index',
-		},
-		rollupOptions: {
-			external: isExternal,
-		},
-	},
+	// build: {
+	// 	target: 'es2015',
+	// 	cssCodeSplit: true,
+	// 	lib: {
+	// 		entry: path.resolve(__dirname, 'src/index.ts'),
+	// 		formats: ['es'],
+	// 		fileName: 'index',
+	// 		name: 'index',
+	// 	},
+	// 	rollupOptions: {
+	// 		external: isExternal,
+	// 	},
+	// },
 	plugins: [
+    react(),
     dts(),
     typescript({
       'target': 'es2020',
