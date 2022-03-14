587f6d35802b89aecd8ea8501ff48207d45f200e
geiger01 <tantangeiger@gmail.com>
2022-03-14 15:51:13 +0200

    STORYBOOK NODERRRR 🥳

---
+++ packages/nindo/src/external/components/tooltip/tooltip.types.ts
@@ -0,0 +1,9 @@
+export interface ITooltipProps {
+	content: string;
+	direction?: 'bottom' | 'left' | 'right' | 'top';
+	pointer?: string;
+	width?: number;
+	backgroundColor?: string;
+	textColor?: string;
+	arrowColor?: string;
+}
