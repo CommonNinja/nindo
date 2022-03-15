d40be352816b2060adad8709148ff61ff3c208f5
geiger01 <tantangeiger@gmail.com>
2022-03-15 17:31:17 +0200

    some more changes (redux error still occurs)

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -12,3 +12,4 @@ module.exports = {
 	],
 	framework: '@storybook/react',
 };
+
--- packages/storybook/stories/assetsGalleryOpener.stories.tsx
+++ packages/storybook/stories/assetsGalleryOpener.stories.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { AssetsGalleryOpener } from '../../nindo/src/external/components/assetsGalleryOpener'
+import React, { useState } from 'react';
+import dedent from 'ts-dedent';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faImage } from '@fortawesome/free-solid-svg-icons'
+
+import { AssetsGalleryOpener } from '../../nindo/src/external/components/assetsGalleryOpener';
+import { FormRow } from '../../nindo/src/external/components/formRow';
+import { ComponentStory } from '@storybook/react';
+// import { AssetType } from '../../nindo/src';
 
 export default {
-  title: 'Editor/Assets Gallery Opener',
+  title: 'Editor/Assets Gallery',
   component: AssetsGalleryOpener,
-
-} as ComponentMeta<typeof AssetsGalleryOpener>;
-
-const Template: ComponentStory<typeof AssetsGalleryOpener> = (args) => (
-  <AssetsGalleryOpener {...args} />
-);
-
-export const assetsGalleryOpener = Template.bind({});
-
-assetsGalleryOpener.args = {
-  enabled: true,
-  submitCallback: () => alert('s')
+  parameters: {
+    docs: {
+      description: {
+        component: dedent(
+          `
+          The \`<AssetsGalleryOpener />\` allows you to open the Common Ninja assets gallery where users may upload, delete, and find images they added to their apps.
+          `
+        ),
+      },
+    },
+  },
 };
 
-//TODO
+const Template: ComponentStory<typeof AssetsGalleryOpener> = (args) => {
+  const [url, setUrl] = useState('');
+  return (
+    <FormRow>
+      <label>Image URL</label>
+      <AssetsGalleryOpener
+        {...args}
+        submitCallback={(url) => setUrl(url)}
+        // assetType='image'
+        enabled={true}
+        disabledCallback={() => alert('hey')}
+      >
+        {/* <FontAwesomeIcon icon={faImage} /> */}
+        <h1>HAHA</h1>
+      </AssetsGalleryOpener>
+      <input
+        type="url"
+        placeholder="Enter image URL"
+        value={url}
+        onChange={(e: any) => setUrl(e.target.value)}
+      />
+    </FormRow>
+  );
+};
+
+export const assetsGallery = Template.bind({});
+assetsGallery.args = {
+  enabled: true,
+  submitCallback: (url) => alert(url),
+}
