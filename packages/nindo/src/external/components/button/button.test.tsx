de15efcd6d109e4249ab22cd3468106a593eb556
geiger01 <tantangeiger@gmail.com>
2022-03-27 15:24:57 +0300

    added tests to Button component

--- packages/nindo/src/external/components/button/button.test.tsx
+++ packages/nindo/src/external/components/button/button.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Button } from './button.comp'
+import '@testing-library/jest-dom'
 
-describe('hi', () => {
-    test("bye", () => {
-        expect(true).toBeFalsy()
+describe('Button Tests', () => {
+    test("Checks button text content", () => {
+        render(<Button>Click!</Button>)
+        expect(screen.getByText('Click!')).toBeInTheDocument()
+    })
+
+    test("Checks button class name", () => {
+        const { container } = render(<Button className='my-class'>Click!</Button>)
+        expect(container.firstChild).toHaveClass('my-class')
+    })
+
+    test("Checks button click event", () => {
+        const mockClick = jest.fn();
+        const { getByText } = render((<Button onClick={mockClick}>Click Me!</Button>));
+        fireEvent.click(getByText(/Click Me!/i));
+        expect(mockClick).toHaveBeenCalled();
     })
 })
