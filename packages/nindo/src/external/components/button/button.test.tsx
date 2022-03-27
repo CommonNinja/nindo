f7bfd332786b778bb8374d0778bd421f0f0c718b
geiger01 <tantangeiger@gmail.com>
2022-03-27 09:53:48 +0300

    tests now configured correctly and running

---
+++ packages/nindo/src/external/components/button/button.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Button } from './button.comp'
+
+
+describe('hi', () => {
+    test("bye", () => {
+        expect(true).toBeTruthy()
+    })
+})
