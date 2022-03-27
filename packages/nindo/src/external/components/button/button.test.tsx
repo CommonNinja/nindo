ee7671c9c0790c3e64b859774219f6dea0c0cb6f
geiger01 <tantangeiger@gmail.com>
2022-03-27 16:47:13 +0300

    add semicolons

--- packages/nindo/src/external/components/button/button.test.tsx
+++ packages/nindo/src/external/components/button/button.test.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
-import { Button } from './button.comp'
-import '@testing-library/jest-dom'
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from './button.comp';
+import '@testing-library/jest-dom';
 
 describe('Button Tests', () => {
     test("Checks button text content", () => {
-        render(<Button>Click!</Button>)
-        expect(screen.getByText('Click!')).toBeInTheDocument()
-    })
+        render(<Button>Click!</Button>);
+        expect(screen.getByText('Click!')).toBeInTheDocument();
+    });
 
     test("Checks button class name", () => {
-        const { container } = render(<Button className='my-class'>Click!</Button>)
-        expect(container.firstChild).toHaveClass('my-class')
-    })
+        const { container } = render(<Button className='my-class'>Click!</Button>);
+        expect(container.firstChild).toHaveClass('my-class');
+    });
 
     test("Checks button click event", () => {
         const mockClick = jest.fn();
         const { getByText } = render((<Button onClick={mockClick}>Click Me!</Button>));
         fireEvent.click(getByText(/Click Me!/i));
         expect(mockClick).toHaveBeenCalled();
-    })
-})
+    });
+});
--- packages/nindo/src/external/components/card/card.test.tsx
+++ packages/nindo/src/external/components/card/card.test.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react'
-import { Card } from './card.comp'
-import '@testing-library/jest-dom'
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Card } from './card.comp';
+import '@testing-library/jest-dom';
 
 describe('Card Tests', () => {
     test("Checks Card text content", () => {
-        render(<Card>Im a card!</Card>)
-        expect(screen.getByText('Im a card!')).toBeInTheDocument()
-    })
+        render(<Card>Im a card!</Card>);
+        expect(screen.getByText('Im a card!')).toBeInTheDocument();
+    });
 
     test("Checks Card class name", () => {
-        const { container } = render(<Card className='my-class'></Card>)
-        expect(container.firstChild).toHaveClass('my-class')
-    })
+        const { container } = render(<Card className='my-class'></Card>);
+        expect(container.firstChild).toHaveClass('my-class');
+    });
 
     test("Checks Card click event", () => {
         const mockClick = jest.fn();
         const { getByText } = render((<Card buttonText={'Click Me!!'} onClick={mockClick}>Im a card!</Card>));
         fireEvent.click(getByText(/Click Me!!/i));
         expect(mockClick).toHaveBeenCalled();
-    })
-})
+    });
+});
